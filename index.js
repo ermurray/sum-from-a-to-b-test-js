d7117fe3244296e5ca208286478b90e0e485f98a
ermurray <ermurray31@gmail.com>
2021-02-06 03:04:15 +0000

    solved sum recursion from a to b

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
+// eslint-disable-next-line func-style
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN < toN) {
+
+    return fromN + sum(fromN + 1, toN);
+  }
+  return fromN;
 }
 
 module.exports = sum;
